140b457202df9c4738de328d91beaf0904cd7421
Manfred Steyer <manfred.steyer@gmx.net>
2021-10-23 11:16:13 +0200

    feat(logger): make config optional
    Before, LoggerModule.forRoot needed a LoggerConfig. Now, the config
    is optional. If omitted, a default config with enableDebug: true
    is used.

--- libs/logger/src/lib/logger.module.ts
+++ libs/logger/src/lib/logger.module.ts
@@ -5,6 +5,10 @@ import { LoggerService } from '..';
 import { DefaultLogFormatter, LogFormatter } from './log-formatter';
 import { ConsoleLogAppender, LogAppender } from './log-appender';
 
+const DEFAULT_CONFIG: LoggerConfig = {
+  enableDebug: true
+};
+
 @NgModule({
   providers: [
     LoggerService
@@ -13,13 +17,13 @@ import { ConsoleLogAppender, LogAppender } from './log-appender';
 export class LoggerModule {
 
   // Setup
-  static forRoot(config: LoggerConfig): ModuleWithProviders<LoggerModule> {
+  static forRoot(config?: LoggerConfig): ModuleWithProviders<LoggerModule> {
     return {
       ngModule: LoggerModule,
       providers: [
-        { provide: LoggerConfig, useValue: config },
-        { provide: LogFormatter, useClass: config.formatter ?? DefaultLogFormatter },
-        { provide: LogAppender, useClass: config.appender ?? ConsoleLogAppender }
+        { provide: LoggerConfig, useValue: config ?? DEFAULT_CONFIG },
+        { provide: LogFormatter, useClass: config?.formatter ?? DefaultLogFormatter },
+        { provide: LogAppender, useClass: config?.appender ?? ConsoleLogAppender }
       ]
     }
   }
